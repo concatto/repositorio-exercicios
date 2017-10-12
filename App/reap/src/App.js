0f612eff42c676ffffdfd4ee20bb273af8acd74b
Fernando Concatto <fernandoconcatto@gmail.com>
2017-10-12 14:56:11 -0300

    App React adicionado

---
+++ App/reap/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">R.E.A.P.</h1>
+          <small>Repositório de Exercícios de Algoritmos e Programação</small>
+        </header>
+        <p className="App-intro">
+          Para começar, entre com suas credenciais ou registre-se no sistema.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
