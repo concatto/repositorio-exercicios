4829e2b19c0799d8c7004257a437f11605d31414
Arthur <arthur.ti@edu.univali.br>
2017-10-19 15:19:18 -0200

    Dropdown funcionando

--- App/reap/src/components/LanguageList.js
+++ App/reap/src/components/LanguageList.js
@@ -8,15 +8,21 @@ export default class LanguageList extends React.Component {
         this.state = {title: "Linguagem"};
     }
 
+    handleChange(e){
+        console.log(e);
+        this.setState({title: this.props.languages[e]});
+    }
+
+
     createList(){
 
         const final = [], options = [];
-
+        let counter = 0;
         {this.props.languages.forEach( (element) => {
-            options.push(<MenuItem eventKey={element.id}>{element}</MenuItem>);
+            options.push(<MenuItem eventKey={counter++}>{element}</MenuItem>);
         }, this)}
 
-        final.push(<DropdownButton title={this.state.title} id="bg-nested-dropdown"> {options} </DropdownButton>);
+        final.push(<DropdownButton onSelect={ (e) => this.handleChange(e)}title={this.state.title} id="bg-nested-dropdown"> {options} </DropdownButton>);
 
         return final;
 
