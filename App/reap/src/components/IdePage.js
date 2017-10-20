39cd6eb2776a0a50f708adf2c4169f80e13de2a2
halersson <halerssonparis@hotmail.com>
2017-10-20 17:57:29 -0200

    mechido em algumas coisas na ide mas n ta pronta

--- App/reap/src/components/IdePage.js
+++ App/reap/src/components/IdePage.js
@@ -1,105 +1,63 @@
 import React from 'react';
-import {Panel, FormGroup, FormControl, Button, Row, Col, Thumbnail} from 'react-bootstrap';
+import {Button, Row, Col, Thumbnail} from 'react-bootstrap';
 import DifficultyView from './DifficultyView';
 import CategoryList from './CategoryList'
-
+import TextAreaCode from './TextAreaCode'
 
 export default class IdePage extends React.Component {
 
-	checkSintaxe() {
+	constructor(props) {
+		super(props);
 
+		this.state = {
+			used_tips: 0,
+			max_tips: 5
+		}
 	}
 
-	totalTips() {
-		return 0 + "/" + 5;
-	}
-
-	positionLine() {
-		return "0:0";
-	}
-
-	countLine() {
-		return 0 + " linhas";
-	}
-
-	insertFooter() {
-		return (
-			<Row>
-	        	<Col xs={3}>
-	        		{this.props.language}
-	        	</Col>
-	        	<Col xs={3}>
-	        		{this.countLine()}
-	        	</Col>
-	        	<Col xs={3}>
-					{this.positionLine()}
-	        	</Col>
-	    	</Row>
-		);
-	}
-
-	insertHeader() {
-		return (
-			<Row>
-				<Col xs={3}>
-					<h4>Codigo-Fonte</h4>
-				</Col>
-				<Col xs={3}>
-					<Button>Verificar</Button>
-				</Col>
-				<Col xs={3}>
-					<Button bsStyle="success" disabled> Enviar </Button>
-				</Col>
-			</Row>
-		);
+	handleChange() {
+		this.setState({used_tips: this.used_tips+1});
 	}
 
 	render() {
 		return(
 			<Row>
-            <Col xs={8}>
-            	<div>
-					<Row> 
-						<Col xs={3}>
-							<h3>
-								<h6>Pontos</h6>
-								0033
-							</h3>
-						</Col>
-						<Col xs={7}>
-							<h2> {this.props.titleExercice} </h2>
-						</Col>
-						<Col xs={2}>
-							<h6> Dificuldade </h6>
-							<DifficultyView difficulty={5} />
-						</Col>
-					</Row>
-
-					<Panel header={this.insertHeader()} footer={this.insertFooter()}>
-		    	    	<FormGroup>
-		    	    		<FormControl componentClass="textarea" className="text-nonResize" />
-		    	    	</FormGroup>
-			        </Panel>
-		    	</div>
-         	</Col>
-            <Col xs={4}>
-            	<Thumbnail> 
-            		<h3> Enunciado: </h3>
-            		<p> {this.props.ProblemDescription} </p>
-            		<CategoryList categories={["Dificil", "Sequência", "Numeros"]} />
-            		<p>________________________________________</p>
-
-            		<Row>	
-            			<Col xs={9}>
-            				<h2>Dicas: {this.totalTips()} </h2>
-            			</Col>
-            			<Col xs={3}>
-            				<Button> Dica </Button>
-            			</Col>
-            		</Row>
-            	</Thumbnail>
-            </Col>
-          </Row>
+	            <Col xs={8}>
+	            	<div>
+						<Row> 
+							<Col xs={3}>
+								<h3>
+									<h6>Pontos</h6>
+									{this.props.points}
+								</h3>
+							</Col>
+							<Col xs={7}>
+								<h2> {this.props.titleExercice} </h2>
+							</Col>
+							<Col xs={2}>
+								<h6> Dificuldade </h6>
+								<DifficultyView difficulty={5} />
+							</Col>
+						</Row>
+						<TextAreaCode language={this.props.language}/>
+			    	</div>
+	         	</Col>
+	            <Col xs={4}>
+	            	<Thumbnail> 
+	            		<h3> Enunciado: </h3>
+	            		<p> {this.props.ProblemDescription} </p>
+	            		<CategoryList categories={["Dificil", "Sequência", "Numeros"]} />
+	            		<Row>	
+	            			<Col xs={9}>
+	            				<h2>Dicas: {this.state.used_tips}/{this.state.max_tips} </h2>
+	            			</Col>
+	            			<Col xs={3}>
+	            				<Button onClick={() => {this.handleChange()}}> Dica </Button>
+	            			</Col>
+	            		</Row>
+	            	</Thumbnail>
+	            </Col>
+          	</Row>
 		);
 	}
 }
---
+++ App/reap/src/components/TextAreaCode.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import {Panel, FormGroup, FormControl, Col, Row, Button} from 'react-bootstrap';
+
+export default class TextAreaCode extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			total_Lines: 1,
+			positionX: 0,
+			positionY: 0,
+		}
+	}
+
+
+	countLine(e) {
+		var count = 1;
+		var key = e.which || e.keyCode || e.charCode;
+
+		for (var i = 0; i < e.target.value.length; i++) {
+			if (key === 13) {
+				count = count + 1;
+				this.setState({total_Lines: count});
+			}
+			console.log(key);
+		}			
+	}
+
+	insertFooter() {
+		return (
+			<Row>
+	        	<Col xs={3}>
+	        		{this.props.language}
+	        	</Col>
+	        	<Col xs={3}>
+	        		{this.state.total_Lines}
+	        	</Col>
+	        	<Col xs={3}>
+					{this.state.positionX + ":" + this.state.positionY}
+	        	</Col>
+	    	</Row>
+		);
+	}
+
+	insertHeader() {
+		return (
+			<Row>
+				<Col xs={3}>
+					<h4>Codigo-Fonte</h4>
+				</Col>
+				<Col xs={3}>
+					<Button>Verificar</Button>
+				</Col>
+				<Col xs={3}>
+					<Button bsStyle="success" disabled> Enviar </Button>
+				</Col>
+			</Row>
+		);
+	}
+
+	render() {
+		return (
+			<Panel header={this.insertHeader()} footer={this.insertFooter()}>
+		    	<FormGroup>
+		    		<FormControl
+		    			componentClass="textarea" 
+		    			className="text-nonResize" 
+		    			onKeyPress={(e) => this.countLine(e)} />
+		    	</FormGroup>
+	        </Panel>
+		);
+	}
+
+}
