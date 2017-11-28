c257090298ae343ac576c894809551cbefc0881e
Fernando Concatto <fernandoconcatto@gmail.com>
2017-11-28 02:05:21 -0200

    Experimentos com nodemailer

--- Backend/entities/membership.js
+++ Backend/entities/membership.js
@@ -2,6 +2,7 @@ const db = require("../db");
 const _ = require("lodash/core");
 const auth = require("../auth");
 const User = require("./user");
+const nodemailer = require("nodemailer");
 const Constants = require("../constants");
 const table = "user_room";
 
@@ -50,10 +51,44 @@ module.exports = {
     return this.isMorePrivilegedThan(Constants.Student, params.room_id, params.id).then(result => {
       const token = auth.createToken(tokenData, {expiresIn: "7d"});
 
+      nodemailer.createTestAccount((err, account) => {
+          // create reusable transporter object using the default SMTP transport
+          let transporter = nodemailer.createTransport({
+              host: 'smtp.ethereal.email',
+              port: 587,
+              secure: false, // true for 465, false for other ports
+              auth: {
+                  user: account.user, // generated ethereal user
+                  pass: account.pass  // generated ethereal password
+              }
+          });
+
+          // setup email data with unicode symbols
+          let mailOptions = {
+              from: '"R.E.A.P" <reap@reap.com>', // sender address
+              to: 'fernandoconcatto@gmail.com', // list of receivers
+              subject: 'Hello ✔', // Subject line
+              text: 'Hello world? ' + token, // plain text body
+              html: '<b>Hello world?</b>' // html body
+          };
+
+          // send mail with defined transport object
+          transporter.sendMail(mailOptions, (error, info) => {
+              if (error) {
+                  return console.log(error);
+              }
+              console.log('Message sent: %s', info.messageId);
+              // Preview only available when sending through an Ethereal account
+              console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+              // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@blurdybloop.com>
+              // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+          });
+      });
       console.log("Should be in an email:", token);
 
       return true;
-    });
+    }).catch(console.log);
   },
 
   // The user must already be registered to accept an invitation.
