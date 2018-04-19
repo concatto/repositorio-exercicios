92c569f94d828bf790ab17d0aab16823a7bb99db
halersson <halerssonparis@hotmail.com>
2018-04-19 11:08:28 -0300

    compiler.js quase pronto -OBS funcoes estao para linux, se for utilizar windows tem que adiocionar .o no final dos exec.

--- Backend/compiler.js
+++ Backend/compiler.js
@@ -4,20 +4,23 @@ const uuid = require('uuid');
 var Promise = require('promise');
 
 const _compile = (code, extension) => {
+
+    const fileName = uuid();
+
+    fs.writeFile(`${fileName}.${extension}`, code, (err) => {
+        if (err) return console.log(err);
+    })
+
     if ('c' === extension || 'cpp' === extension) {
-        const fileName = uuid();
-
-        fs.writeFile(`${fileName}.${extension}`, code, (err) => {
-            if (err) return console.log(err);
-
-        })
-
+       
         return new Promise((resolve, reject) => {
-            exec(`g++ ${fileName}.${extension} -o ${fileName}.o`, (err, stdout, stderr) => {
-                if (err) {
+            //add .o
+            console.log(`g++ --std=c++11 ${fileName}.${extension} -o ${fileName}`)
+            exec(`g++ --std=c++11 ${fileName}.${extension} -o ${fileName}`, (err, stdout, stderr) => {
+                if (err || stderr) {
                     console.log(stderr);
                     fs.unlink(`${fileName}.${extension}`);
-                    return stderr;
+                    return err;
                 }
                 resolve(fileName);
             });             
@@ -29,8 +32,8 @@ const compile = (code, extension) => {
 
     _compile(code, extension)
     .then( fileName => {
-        fs.unlink(`${fileName}.${extension}`);
-        fs.unlink(`${fileName}.o`);
+        //add .o
+        deleteFiles(fileName, extension);
     }).catch(stderr => {
         //do something with stderr
     })
@@ -39,39 +42,45 @@ const compile = (code, extension) => {
 const compileAndRun = (code, extension, testCase) => {
     _compile(code, extension)
     .then(fileName => {
-         //console.log(fileName);
-        exec(`${fileName}.o`, (err, stdout, stderr) => {
-            console.log(stdout);
-            console.log(stderr);
-            
-            testCase.forEach(value => {
-              //preencher coisa aqui
+        console.log(fileName);
+        //add .o
+        testCase.forEach(value => {
+            exec(`${fileName} ${value.in}`, (err, stdout, stderr) => {
+                if (!compareCaseTest(stdout, value.out)) {
+                    //return false | "string" ??                    
+                    return;
+                }
             })
-          
-            fs.unlink(`${fileName}.${extension}`);
-            fs.unlink(`${fileName}.o`);
         })
-       
+        deleteFiles(fileName, extension);
     }).catch(stderr => {
         //do something with stderr
     })
 }
 
-const compareCaseTest = (testCase, result) => {
-  //arrumar função comapare test
-    if (testCase == result) {
-      console.log("deu certo!", testCase)
+const compareCaseTest = (result, caseTestOut) => {
+
+    if (caseTestOut == result) {
+      console.log("deu certo!", testCaseOut)
       return true;
     }
     else {
-      console.log("nap deu")
+      console.log("na1 deo")
       return false;
     }
 }
 
+const deleteFiles = (fileName, extension) => {
+    fs.unlink(`${fileName}.${extension}`);
+    fs.unlink(`${fileName}`);
+}
+
 
 //verificar qual entrada do caso de test
-compileAndRun(`#include <iostream> \n int main(){std::cout<<"Sua Mae"; return 1;}`, 'cpp', [{in: 5, out: 120}]);
+
+compileAndRun(`#include <iostream> \nint main(){std::cout<<"5"; return 1;}`, 'cpp', [{in: 5, out: 7}]);
+
+//compile(`#include <iostream> \n int main(){std::cout<<"5"; return 1;}`, 'cpp');
 
 module.exports =
     {
