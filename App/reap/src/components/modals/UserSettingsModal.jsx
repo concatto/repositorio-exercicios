eca1a62f867e9d221abeffe16164514f63bf58b4
unknown <andrielferreira@outlook.com.br>
2018-05-22 22:11:53 -0300

    Adicionado dropdown para selecao de novo cargo, mostrando informacao correta de cargo no modal

--- App/reap/src/components/modals/UserSettingsModal.jsx
+++ App/reap/src/components/modals/UserSettingsModal.jsx
@@ -2,12 +2,26 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Modal } from 'react-bootstrap';
 
-
 class UserSettingsModal extends React.Component {
   render() {
     const { onDismiss, user, roomId } = this.props;
     const { id, name, experience, privilege, joined_at } = user;
-
+    var privilegeVerify;
+      
+      switch({privilege}.privilege){
+          case 0:
+              privilegeVerify='Criador da Sala';
+              break;
+          case 1:
+              privilegeVerify='Administrador';
+              break;
+          case 2:
+              privilegeVerify='Professor';
+              break;
+          case 3:
+              privilegeVerify='Aluno';
+              break;
+      }
     return (
       <div>
         <Modal.Header closeButton>
@@ -16,8 +30,16 @@ class UserSettingsModal extends React.Component {
         <Modal.Body>
             <div> 
                 <h3> Nome: {name} </h3> 
-                <h4> Privilégio atual: {privilege} </h4> 
+                <h4> Privilégio atual: {privilegeVerify} </h4> 
+                <br></br>
+               
             </div>
+<select id="select">
+  <option value="1">Administrador</option>
+  <option value="2">Professor</option>
+  <option value="3">Aluno</option>
+</select>
+            <button id="userSettingButton" class="atualizar">OK</button>
         </Modal.Body>
       </div>
     );
